b21df5ad3f71bdddd6a4ce1df6e43eb7b013799a
nowellcode <159472500+nowellcode@users.noreply.github.com>
2024-03-20 12:11:57 +0000

    dummy page attempt

---
+++ src/app/diveomega/page.tsx
@@ -0,0 +1,43 @@
+import { Footer } from "../components/Footer";
+import { Menu } from "../components/Menu";
+import { WatchCard } from "../components/WatchCard";
+import React from "react";
+import watch9 from "../../../assets/watch9.png";
+
+export default function Dive() {
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="flex flex-row">
+        <Menu />
+        <h1>HELLO</h1>
+        <WatchCard image={watch9} title="Omega Seamaster 300m" price={5300} />
+        <p>
+          OMEGA Seamaster Diver 300m 42mm White Dial Men's Rubber Strap Watch
+          <br />
+          The Seamaster Diver 300M watch has been enjoying a loyal following
+          since the first model from the series was launched by OMEGA back in
+          1993. To this day, its robust casing, legible dial and accurate
+          movement have served the wrists of loyal fans all over the world.
+          Geared towards the tastes and needs of the modern dive enthusiast, the
+          OMEGA Seamaster Diver 300M watch measures a 42mm diameter and Features
+          a scratch resistant and anti reflective sapphire crystal glass front.
+          Its unidirectional rotating black ceramic bezel with a white enamel
+          diving scale surrounds a polished white ceramic dial with
+          laser-engraved waves and Super-LumiNova filled hands and indexes.
+          <br />
+          The Seamaster lettering and tip of the second hand is highlighted in
+          red, while a date aperture can be found at 6 o’clock. Inside the
+          300-meter water resistant steel case of this OMEGA Seamaster Diver
+          300M watch is the Master Chronometer Calibre 8800, which promises a
+          55-hour power reserve. The model also Features a helium escape valve
+          as well as a transparent sapphire crystal glass caseback.
+          <br />
+          To complete its sporty water-ready aesthetic, the OMEGA Seamaster
+          Diver 300M watch wraps comfortably around the wrist with a black
+          rubber strap and a robust steel fastening.{" "}
+        </p>
+      </div>
+      <Footer />
+    </div>
+  );
+}
