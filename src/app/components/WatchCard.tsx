1168a08210b69e186ba2785b9313cd6d06a26d68
kingzurley <kingsley.campbell.hunter@gmail.com>
2024-03-24 17:33:57 +0000

    add watch card padding and pound sign

--- src/app/components/WatchCard.tsx
+++ src/app/components/WatchCard.tsx
@@ -8,10 +8,10 @@ interface IWatchCardProps {
 
 export const WatchCard = (props: IWatchCardProps): JSX.Element => {
   return (
-    <div className="flex flex-col items-center gap-3 bg-white shadow-md font-bold h-96 w-72 hover:scale-105 duration-200 rounded-md">
+    <div className="flex flex-col items-center gap-3 bg-white shadow-md font-bold h-96 w-72 hover:scale-105 duration-200 rounded-md p-3">
       <Image src={props.image} height={200} width={270} alt={props.title} />
       <h1>{props.title}</h1>
-      <h2>{props.price}</h2>
+      <h2>£{props.price}</h2>
     </div>
   );
 };
