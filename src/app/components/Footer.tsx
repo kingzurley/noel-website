d0aa864ef608b3fd441466c19eb060ff93ea0042
kingzurley <kingsley.campbell.hunter@gmail.com>
2024-03-24 17:34:26 +0000

    add some dummy data to footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,14 @@
 export const Footer = (): JSX.Element => {
   return (
-    <div className="bg-slate-50 h-24 bottom-0 w-screen">
-      <text>Hello footer Copyright Notice Logo Socials/Contact info</text>
+    <div className="bg-slate-50 h-52 bottom-0 w-screen p-5">
+      <div className="grid grid-cols-2 h-full content-center">
+        <text>Hello footer Copyright Notice Logo Socials/Contact info</text>
+        <div className="grid grid-cols-3">
+          <text>Copyright Notice Logo Socials/Contact info</text>
+          <text>Copyright Notice Logo Socials/Contact info</text>
+          <text>Copyright Notice Logo Socials/Contact info</text>
+        </div>
+      </div>
     </div>
   );
 };
