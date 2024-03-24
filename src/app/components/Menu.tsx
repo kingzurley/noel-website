19cb84741b17fbc11360626752a95a2b3534fddd
kingzurley <kingsley.campbell.hunter@gmail.com>
2024-03-24 14:17:24 +0000

    make menu more fun

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -1,12 +1,35 @@
+"use client";
+
+import { useState } from "react";
 import { MenuItem } from "./MenuItem";
 
+const menuItems = [
+  { title: "Daily", link: "/" },
+  { title: "Dive", link: "/dive" },
+  { title: "Dress", link: "/dress" },
+  { title: "Pilot", link: "/pilot" },
+];
+
 export const Menu = (): JSX.Element => {
+  const [activeButtonIndex, setActiveButtonIndex] = useState(0);
+
   return (
-    <div className="min-w-64 flex flex-col gap-4 bg-transparent bg-slate-100 sticky top-0 h-screen">
-      <MenuItem title="Daily" link="/" />
-      <MenuItem title="Dive" link="/dive" />
-      <MenuItem title="Dress" link="/dress" />
-      <MenuItem title="Pilot" link="/pilot" />
+    <div className="flex flex-col gap-4 bg-transparent bg-slate-100 sticky top-0 h-screen p-5">
+      {menuItems.map((item, key) => {
+        return (
+          <button
+            key={key}
+            onClick={() => setActiveButtonIndex(key)}
+            className={`${
+              activeButtonIndex === key
+                ? "bg-violet-400 text-white"
+                : "bg-white text-black"
+            } rounded-lg hover:bg-violet-400 duration-500`}
+          >
+            <MenuItem title={item.title} link={item.link} />
+          </button>
+        );
+      })}
     </div>
   );
 };
