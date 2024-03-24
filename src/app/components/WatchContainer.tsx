05ff93d0457efb7b5c420bdcd4f5da9611ddac5e
kingzurley <kingsley.campbell.hunter@gmail.com>
2024-03-24 17:34:08 +0000

    removed shadown from watch container

--- src/app/components/WatchContainer.tsx
+++ src/app/components/WatchContainer.tsx
@@ -14,7 +14,7 @@ export const WatchContainer = (
   props: IWatchContainerInterface
 ): JSX.Element => {
   return (
-    <div className="col-span-4 flex flex-row justify-center font-mono gap-10 flex-wrap pt-10 text-base shadow-md bg-gradient-to-t from-slate-50 to-slate-200 hover:bg-red-700 h-full">
+    <div className="col-span-4 flex flex-row justify-center font-mono gap-10 flex-wrap pt-10 text-base bg-gradient-to-t from-slate-50 to-slate-200 hover:bg-red-700 h-full">
       {props.model.map((watch: IWatchModel, index) => {
         return (
           <WatchCard
